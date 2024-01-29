5b40f78b5abb4accd305f2ffef0706ed6f02a6b0
kobi1108 <140871439+kobi1108@users.noreply.github.com>
2024-01-29 20:32:14 +0800

    Update GithubProject.vue

--- src/components/GithubProject.vue
+++ src/components/GithubProject.vue
@@ -45,29 +45,10 @@ import { Pagination, Mousewheel } from "swiper";
 const projectData = [
   {
     name: "home",
-    author: "imsyy",
-    desc: "个人主页，我的个人主页，个人主页源码，主页模板，homepage",
+    author: "rinshiko",
+    desc: "個人主頁，我的個人主頁，個人主頁原始碼，主頁模板，homepage",
   },
-  {
-    name: "SPlayer",
-    author: "imsyy",
-    desc: "🎉 一个简约的音乐播放器，支持网易云音乐账号登录，逐字歌词，下载歌曲，展示评论区，音乐云盘及歌单管理，音乐频谱，移动端基础适配 | A minimalist music player",
-  },
-  {
-    name: "Snavigation",
-    author: "imsyy",
-    desc: "Snavigation 一个简约的起始页 | 支持自定义搜索引擎，自定义快捷方式，自定义壁纸以及数据备份",
-  },
-  {
-    name: "DailyHotApi",
-    author: "imsyy",
-    desc: "今日热榜 API，一个聚合热门数据的 API 接口，支持 Vercel 部署 | 前端页面：https://github.com/imsyy/DailyHot",
-  },
-  {
-    name: "site-status",
-    author: "imsyy",
-    desc: "📺 一个基于 UptimeRobot API 的在线状态面板 | 站点监测 | 状态检测 | An online status panel based on the UptimeRobot API | UptimeRobot, status, site",
-  },
+
 ];
 
 // 计算网站链接
