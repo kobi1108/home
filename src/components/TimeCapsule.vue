b510284de2d6f19af5e79f006305cd879ceea61a
kobi1108 <140871439+kobi1108@users.noreply.github.com>
2024-01-29 20:00:41 +0800

    Update TimeCapsule.vue

--- src/components/TimeCapsule.vue
+++ src/components/TimeCapsule.vue
@@ -2,15 +2,15 @@
   <div class="time-capsule">
     <div class="title">
       <hourglass-full theme="two-tone" size="24" :fill="['#efefef', '#00000020']" />
-      <span>时光胶囊</span>
+      <span>時光膠囊</span>
     </div>
-    <span class="text">今日已经度过了&nbsp;{{ timeData.day.elapsed }}&nbsp;小时</span>
+    <span class="text">今日已經度過了&nbsp;{{ timeData.day.elapsed }}&nbsp;小时</span>
     <el-progress :text-inside="true" :stroke-width="20" :percentage="timeData.day.pass" />
-    <span class="text">本周已经度过了&nbsp;{{ timeData.week.elapsed }}&nbsp;天</span>
+    <span class="text">本週已經度過了&nbsp;{{ timeData.week.elapsed }}&nbsp;天</span>
     <el-progress :text-inside="true" :stroke-width="20" :percentage="timeData.week.pass" />
-    <span class="text">本月已经度过了&nbsp;{{ timeData.month.elapsed }}&nbsp;天</span>
+    <span class="text">本月已經度過了&nbsp;{{ timeData.month.elapsed }}&nbsp;天</span>
     <el-progress :text-inside="true" :stroke-width="20" :percentage="timeData.month.pass" />
-    <span class="text">今年已经度过了&nbsp;{{ timeData.year.elapsed }}&nbsp;个月</span>
+    <span class="text">今年已經度過了&nbsp;{{ timeData.year.elapsed }}&nbsp;个月</span>
     <el-progress :text-inside="true" :stroke-width="20" :percentage="timeData.year.pass" />
     <div v-if="startDate?.length >= 4 && store.siteStartShow">
       <span class="text" v-html="startDateText" />
@@ -31,7 +31,7 @@ import { getTimeCapsule, siteDateStatistics } from "@/utils/getTime.js";
 import { mainStore } from "@/store";
 const store = mainStore();
 
-// 进度条数据
+// 進度條數據
 const timeData = ref(getTimeCapsule());
 const startDate = ref(import.meta.env.VITE_SITE_START);
 const startDateText = ref(null);
