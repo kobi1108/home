52901051ab200a79e9402054d5242d6514959a87
kobi1108 <140871439+kobi1108@users.noreply.github.com>
2024-01-30 11:51:47 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, AiResults, CompassOutline, ClockAlarm24Regular, AppsListDetail24Regular, BookOpen24Regular    } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, AiResults, CompassOutline, ClockAlarm24Regular, AppsListDetail24Regular, BookOpen24Regular } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -61,7 +61,7 @@ const siteLinksList = computed(() => {
   return result;
 });
 
-// 网站链接图标
+// 网站链接圖標
 const siteIcon = {
   Blog,
   AiResults,
