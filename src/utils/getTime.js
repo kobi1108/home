f8fe433e51c5fcad4d53231821d05828ef0f36cd
kobi1108 <140871439+kobi1108@users.noreply.github.com>
2024-02-01 20:58:45 +0800

    Update getTime.js

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -74,25 +74,25 @@ export const helloInit = () => {
   const hour = new Date().getHours();
   let hello = null;
   if (hour < 6) {
-    hello = "凌晨好";
+    hello = "凌晨好 ";
   } else if (hour < 9) {
-    hello = "早上好";
+    hello = "早上好 ";
   } else if (hour < 12) {
-    hello = "上午好";
+    hello = "上午好 ";
   } else if (hour < 14) {
-    hello = "中午好";
+    hello = "中午好 ";
   } else if (hour < 17) {
-    hello = "下午好";
+    hello = "下午好 ";
   } else if (hour < 19) {
-    hello = "傍晚好";
+    hello = "傍晚好 ";
   } else if (hour < 22) {
-    hello = "晚上好";
+    hello = "晚上好 ";
   } else {
-    hello = "夜深了";
+    hello = "夜深了 ";
   }
   ElMessage({
     dangerouslyUseHTMLString: true,
-    message: `<strong>${hello}</strong>  歡迎來到我的主頁`,
+    message: `<strong>${hello}</strong>  -迎來到我的主頁`,
   });
 };
 
